b0a3b06afb46aa879ace558934867e4f88fe538e
ste <mrbarrow@hotmail.co.uk>
2025-12-04 18:04:46 +0000

    initial commit

---
+++ src/main/java/com/yanader/task_backend/TaskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yanader.task_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yanader/task_backend/TaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanader.task_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
