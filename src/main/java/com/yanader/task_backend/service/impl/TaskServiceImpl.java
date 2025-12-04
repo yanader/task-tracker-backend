66e09be5a9f8614b7ef24c15b70413574a6e6af9
ste <mrbarrow@hotmail.co.uk>
2025-12-04 21:44:48 +0000

    fix:added service annotation to service impl

--- src/main/java/com/yanader/task_backend/service/impl/TaskServiceImpl.java
+++ src/main/java/com/yanader/task_backend/service/impl/TaskServiceImpl.java
@@ -5,7 +5,9 @@ import com.yanader.task_backend.model.dtos.TaskDTO;
 import com.yanader.task_backend.repository.TaskRepository;
 import com.yanader.task_backend.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TaskServiceImpl implements TaskService {
 
     @Autowired
