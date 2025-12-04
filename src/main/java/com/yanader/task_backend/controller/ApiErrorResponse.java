94f4c3bbe0a3f19819c305471e0c92bd50c2e3ce
ste <mrbarrow@hotmail.co.uk>
2025-12-04 21:03:11 +0000

    feat:ApiErrorResponse

---
+++ src/main/java/com/yanader/task_backend/controller/ApiErrorResponse.java
@@ -0,0 +1,27 @@
+package com.yanader.task_backend.controller;
+
+public class ApiErrorResponse {
+    private int status;
+    private String message;
+
+    public ApiErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
